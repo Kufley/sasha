b2ff86751cdc5bddf8fb280e0968e92cd6f0ef58
Yura Mozol <yura.mozol@gmail.com>
2017-05-25 16:23:46 +0300

    changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,86 @@
+function initGallery() {
+    var $gallery = $('.container-gallery'),
+        index = 0;
+
+    $('.wrap-slide', $gallery).each(function () {
+        $(this).data('imageIndex', index).addClass('image-index-' + index);
+        index++;
+    });
+
+    $gallery.data('countImages', index);
+
+    $('.wrap-slide', $gallery).click(function () {
+        $gallery.data('currentIndex', $(this).data('imageIndex'));
+        $('.gallery-body').empty();
+        $($(this).html()).appendTo('.gallery-body');
+        $('.slide-gallary').show();
+        $('.overlay').addClass('have');
+
+        if ($(window).width() < 1199) {
+            $('.slide-gallary').show().addClass('fixed');
+            $('.overlay').addClass('fixed');
+        }
+
+    });
+    $('.close').click(function () {
+        $('.slide-gallary').hide();
+        $('.overlay').removeClass('have');
+
+    });
+
+    $('.overlay').click(function () {
+        $('.slide-gallary').hide();
+        $('.overlay').removeClass('have');
+
+    });
+
+
+    $('.next').click(function () {
+        nextImage(1);
+    });
+
+    $('.prev').click(function () {
+        nextImage(-1);
+    });
+
+    function nextImage(d) {
+        var index = $gallery.data('currentIndex') + d,
+            countImages = $gallery.data('countImages');
+
+        if (index < 0) {
+            index = countImages - 1;
+        }
+
+        if (index >= countImages) {
+            index = 0;
+        }
+
+        $('.wrap-slide.image-index-' + index, $gallery).trigger('click');
+    }
+}
+
+
 $(document).ready(function(){
 
-
-    $('#slider-main').carousel({
+    $('#myCarousel').carousel({
         interval: 0
+    })
+
+    $('.carousel .item').each(function(){
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        for (var i=0;i<5;i++) {
+            next=next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+
+            next.children(':first-child').clone().appendTo($(this));
+        }
     });
     var parPosition = [];
     $('.block').each(function() {
@@ -63,149 +141,7 @@ $(document).ready(function(){
 
 
 
+    initGallery();
 
-    $('#slider-team').carousel({
-        interval: 0
-    });
-    (function(){
-        $('#slider-team .item').each(function(){
-            var itemToClone = $(this).append('<div></div>');
-
-            for (var i=1;i<3;i++) {
-                itemToClone = itemToClone.next();
-
-                // wrap around if at end of item collection
-                if (!itemToClone.length) {
-                    itemToClone = $(this).siblings(':first');
-                }
-
-                // grab item, clone, add marker class, add to collection
-                itemToClone.children(':first-child').clone()
-                    .addClass("cloneditem-"+(i))
-                    .appendTo($(this));
-            }
-        });
-    }());
-    // $('#slider-team.carousel .item').each(function(){
-    //     var next = $(this).next();
-    //     if (!next.length) {
-    //
-    //         next = $(this).siblings(':first');
-    //     }
-    //     next.children(':first-child').clone().appendTo($(this));
-    //
-    //     if (next.next().length>0) {
-    //         next.next().children(':first-child').clone().appendTo($(this));
-    //     }
-    //     else {
-    //         $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-    //     }
-    //
-    // });
-    // $('#slider-team').on('slid.bs.carousel', function () {
-    //     $(".item.active:nth-child(" + ($(".carousel-inner .item").length -1) + ") + .item").insertBefore($(".item:first-child"));
-    //     $(".item.active:last-child").insertBefore($(".item:first-child"));
-    // });
-    var $carousel = $('.carousel1');
-    var $seats = $('.carousel-seat');
-
-    $('.toggle').on('click', function(e) {
-        var $newSeat;
-        var $el = $('.is-ref');
-        var $currSliderControl = $(e.currentTarget);
-        // Info: e.target is what triggers the event dispatcher to trigger and e.currentTarget is what you assigned your listener to.
-
-        $el.removeClass('is-ref');
-        if ($currSliderControl.data('toggle') === 'next') {
-            $newSeat = next($el);
-            $carousel.removeClass('is-reversing');
-        } else {
-            $newSeat = prev($el);
-            $carousel.addClass('is-reversing');
-        }
-
-        $newSeat.addClass('is-ref').css('order', 1);
-        for (var i = 2; i <= $seats.length; i++) {
-            $newSeat = next($newSeat).css('order', i);
-        }
-
-        $carousel.removeClass('is-set');
-        return setTimeout(function() {
-            return $carousel.addClass('is-set');
-        }, 50);
-
-        function next($el) {
-            if ($el.next().length) {
-                return $el.next();
-            } else {
-                return $seats.first();
-            }
-        }
-
-        function prev($el) {
-            if ($el.prev().length) {
-                return $el.prev();
-            } else {
-                return $seats.last();
-            }
-        }
-    });
-
-});
-
-function moveToSelected(element) {
-
-    if (element == "next") {
-        var selected = $(".selected").next();
-    } else if (element == "prev") {
-        var selected = $(".selected").prev();
-    } else {
-        var selected = element;
-    }
-
-    var next = $(selected).next();
-    var prev = $(selected).prev();
-    var prevSecond = $(prev).prev();
-    var nextSecond = $(next).next();
-
-    $(selected).removeClass().addClass("selected");
-
-    $(prev).removeClass().addClass("prev");
-    $(next).removeClass().addClass("next");
-
-    $(nextSecond).removeClass().addClass("nextRightSecond");
-    $(prevSecond).removeClass().addClass("prevLeftSecond");
-
-    $(nextSecond).nextAll().removeClass().addClass('hideRight');
-    $(prevSecond).prevAll().removeClass().addClass('hideLeft');
-
-}
-
-
-$(document).keydown(function(e) {
-    switch(e.which) {
-        case 37: // left
-            moveToSelected('prev');
-            break;
-
-        case 39: // right
-            moveToSelected('next');
-            break;
-
-        default: return;
-    }
-    e.preventDefault();
-});
-
-$('#carousel .img').click(function() {
-    moveToSelected($(this));
-});
-
-$('#prev').click(function() {
-    moveToSelected('prev');
-});
-
-$('#next').click(function() {
-    moveToSelected('next');
 });
 
